ea009435facc2a2feb199f30893c1c2a3f22636a
Amil Khan <amil@ucsb.edu>
2024-02-15 17:35:54 -0800

    Update docusaurus.config.js

--- ece-178-notes/docusaurus.config.js
+++ ece-178-notes/docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://amilworks.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ece178/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
